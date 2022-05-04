734a764889221c1106b22b023281b6696a301db5
Hgxegz1 <hgxegz21@gmail.com>
2022-05-03 20:14:40 -0700

    file for the motor functions

---
+++ wheelControl/MotorModule.py
@@ -0,0 +1,59 @@
+import RPI.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+class Motor():
+    def __init__(self, in1, in2, in3, in4):
+        self.in1 = in1
+        self.in2 = in2
+        self.in3 = in3
+        self.in4 = in4
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
+        
+    def moveForward(self):
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+        
+    def moveBackward(self):
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
+        
+    def moveLeft(self):
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+        
+    def moveRight(self):
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
+        
+    def stop(self);
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.LOW)
+
+in1 = 24
+in2 = 23
+in3 = 17
+in4 = 27
+
+temp1 = 1
+
+motors = Motor(in1, in2, in3, in4)
+motors.moveForward()
+motors.moveBackward()
+motors.moveLeft()
+motors.moveRight()
+
+
